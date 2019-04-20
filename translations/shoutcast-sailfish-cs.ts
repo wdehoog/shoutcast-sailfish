0388e912785177e820793f4e1f4b79df6629aa69
wdehoog <wdehoog@exalondelft.nl>
2019-04-21 00:19:11 +0200

    remove the type="unfinished" attributes from all translated entries

--- translations/shoutcast-sailfish-cs.ts
+++ translations/shoutcast-sailfish-cs.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="cs_CZ" sourcelanguage="cs_CZ">
+<TS version="2.1" language="cs">
 <context>
     <name></name>
     <message id="Shoutcast for SailfishOS">
@@ -12,15 +12,15 @@
     <name>AboutPage</name>
     <message>
         <source>About</source>
-        <translation type="unfinished">O aplikaci</translation>
+        <translation>O aplikaci</translation>
     </message>
     <message>
         <source>Shoutcast player for Sailfish OS</source>
-        <translation type="unfinished">Přehrávač Shoutcast pro Sailfish OS</translation>
+        <translation>Přehrávač Shoutcast pro Sailfish OS</translation>
     </message>
     <message>
         <source>Thanks to</source>
-        <translation type="unfinished">Poděkování</translation>
+        <translation>Poděkování</translation>
     </message>
     <message>
         <source>License: MIT</source>
@@ -47,11 +47,11 @@
     <name>GenrePage</name>
     <message>
         <source>Genres</source>
-        <translation type="unfinished">Žánr</translation>
+        <translation>Žánr</translation>
     </message>
     <message>
         <source>No Station Name</source>
-        <translation type="unfinished">Stanice bez názvu</translation>
+        <translation>Stanice bez názvu</translation>
     </message>
     <message>
         <source>?</source>
@@ -59,11 +59,11 @@
     </message>
     <message>
         <source>Reload</source>
-        <translation type="unfinished">Obnovit</translation>
+        <translation>Obnovit</translation>
     </message>
     <message>
         <source>SHOUTcast server did not respond</source>
-        <translation type="unfinished">Server Shoutcast neodpovídá</translation>
+        <translation>Server Shoutcast neodpovídá</translation>
     </message>
 </context>
 <context>
@@ -74,18 +74,18 @@
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished">Zrušit</translation>
+        <translation>Zrušit</translation>
     </message>
 </context>
 <context>
     <name>MainPage</name>
     <message>
         <source>About</source>
-        <translation type="unfinished">O aplikaci</translation>
+        <translation>O aplikaci</translation>
     </message>
     <message>
         <source>Settings</source>
-        <translation type="unfinished">Nastavení</translation>
+        <translation>Nastavení</translation>
     </message>
     <message>
         <source>Shoutcast Sailfish</source>
@@ -93,11 +93,11 @@
     </message>
     <message>
         <source>Genres</source>
-        <translation type="unfinished">Žánr</translation>
+        <translation>Žánr</translation>
     </message>
     <message>
         <source>Search</source>
-        <translation type="unfinished">Hledat</translation>
+        <translation>Hledat</translation>
     </message>
     <message>
         <source>Top 500</source>
@@ -105,113 +105,113 @@
     </message>
     <message>
         <source>There is no Network available.</source>
-        <translation type="unfinished">Není k dispozici síťové připojení.</translation>
+        <translation>Není k dispozici síťové připojení.</translation>
     </message>
 </context>
 <context>
     <name>Search</name>
     <message>
         <source>Search</source>
-        <translation type="unfinished">Hledat</translation>
+        <translation>Hledat</translation>
     </message>
     <message>
         <source>Search text</source>
-        <translation type="unfinished">Hledat text</translation>
+        <translation>Hledat text</translation>
     </message>
     <message>
         <source>Search in</source>
-        <translation type="unfinished">Hledat v</translation>
+        <translation>Hledat v</translation>
     </message>
     <message>
         <source>Now Playing</source>
-        <translation type="unfinished">Právě hraje</translation>
+        <translation>Právě hraje</translation>
     </message>
     <message>
         <source>Keyword</source>
-        <translation type="unfinished">Klíčové slovo</translation>
+        <translation>Klíčové slovo</translation>
     </message>
     <message>
         <source>No stations found</source>
-        <translation type="unfinished">Stanice nenalezeny</translation>
+        <translation>Stanice nenalezeny</translation>
     </message>
     <message>
         <source>Show Player</source>
-        <translation type="unfinished">Zobrazit přehrávač</translation>
+        <translation>Zobrazit přehrávač</translation>
     </message>
     <message>
         <source>SHOUTcast server did not respond</source>
-        <translation type="unfinished">Server Shoutcast neodpovídá</translation>
+        <translation>Server Shoutcast neodpovídá</translation>
     </message>
 </context>
 <context>
     <name>SettingsPage</name>
     <message>
         <source>Settings</source>
-        <translation type="unfinished">Nastavení</translation>
+        <translation>Nastavení</translation>
     </message>
     <message>
         <source>Maximum number of results per request</source>
-        <translation type="unfinished">Maximální počet výsledků žádosti</translation>
+        <translation>Maximální počet výsledků žádosti</translation>
     </message>
     <message>
         <source>Player Type</source>
-        <translation type="unfinished">Typ přehrávače</translation>
+        <translation>Typ přehrávače</translation>
     </message>
     <message>
         <source>Which type of player to use</source>
-        <translation type="unfinished">Který typ přehrávače použít</translation>
+        <translation>Který typ přehrávače použít</translation>
     </message>
     <message>
         <source>Built In Player (Qt Audio)</source>
-        <translation type="unfinished">Vestavěný (Qt Audio)</translation>
+        <translation>Vestavěný (Qt Audio)</translation>
     </message>
     <message>
         <source>Mpris Player</source>
-        <translation type="unfinished">Přehrávač Mpris</translation>
+        <translation>Přehrávač Mpris</translation>
     </message>
     <message>
         <source>Detect Mpris Players</source>
-        <translation type="unfinished">Detekovat Mpris přehrávač</translation>
+        <translation>Detekovat Mpris přehrávač</translation>
     </message>
     <message>
         <source>Mpris Service Name (skip &apos;org.mpris.MediaPlayer2&apos;)</source>
-        <translation type="unfinished">Název služby Mpris  (přeskočte &apos;org.mpris.MediaPlayer2&apos;)</translation>
+        <translation>Název služby Mpris  (přeskočte &apos;org.mpris.MediaPlayer2&apos;)</translation>
     </message>
     <message>
         <source>Mpris Players</source>
-        <translation type="unfinished">Mpris přehrávače</translation>
+        <translation>Mpris přehrávače</translation>
     </message>
     <message>
         <source>Mime Type Filter</source>
-        <translation type="unfinished">MIME typ filtr</translation>
+        <translation>MIME typ filtr</translation>
     </message>
     <message>
         <source>Which mime types to list</source>
-        <translation type="unfinished">Které MIME typy zobrazovat</translation>
+        <translation>Které MIME typy zobrazovat</translation>
     </message>
     <message>
         <source>No filter. Accept all mime types.</source>
-        <translation type="unfinished">Bez filtru. Zobrazovat všechny typy.</translation>
+        <translation>Bez filtru. Zobrazovat všechny typy.</translation>
     </message>
     <message>
         <source>Accept only MP3 (audio/mpeg)</source>
-        <translation type="unfinished">Přijmout pouze MP3 (audio/mpeg)</translation>
+        <translation>Přijmout pouze MP3 (audio/mpeg)</translation>
     </message>
     <message>
         <source>Accept only AAC (audio/aacp)</source>
-        <translation type="unfinished">Přimout pouze AAC (audio/aacp)</translation>
+        <translation>Přimout pouze AAC (audio/aacp)</translation>
     </message>
     <message>
         <source>Timeout for shoutcast server queries (seconds)</source>
-        <translation type="unfinished">Timeout pro Shoutcast server (sekundy)</translation>
+        <translation>Timeout pro Shoutcast server (sekundy)</translation>
     </message>
     <message>
         <source>Buffered percentage before play starts</source>
-        <translation type="unfinished">Počet procent v bufferu před spuštěním přehrávání</translation>
+        <translation>Počet procent v bufferu před spuštěním přehrávání</translation>
     </message>
     <message>
         <source>Start playing when buffered percentage is at set level or start playing immediately.</source>
-        <translation type="unfinished">Spustit přehrávání ihned nebo po dosažení nastavených procent v bufferu.</translation>
+        <translation>Spustit přehrávání ihned nebo po dosažení nastavených procent v bufferu.</translation>
     </message>
     <message>
         <source>Start at buffered percentage</source>
@@ -222,56 +222,56 @@
     <name>StationListItemView</name>
     <message>
         <source>no track info</source>
-        <translation type="unfinished">Skladba bez informací</translation>
+        <translation>Skladba bez informací</translation>
     </message>
 </context>
 <context>
     <name>StationsPage</name>
     <message>
         <source>Reload</source>
-        <translation type="unfinished">Obnovit</translation>
+        <translation>Obnovit</translation>
     </message>
     <message>
         <source>No stations found</source>
-        <translation type="unfinished">Stanice nenalezeny</translation>
+        <translation>Stanice nenalezeny</translation>
     </message>
     <message>
         <source>SHOUTcast server returned no Stations</source>
-        <translation type="unfinished">SHOUTcast server nenalezl stanice</translation>
+        <translation>SHOUTcast server nenalezl stanice</translation>
     </message>
     <message>
         <source>Show Player</source>
-        <translation type="unfinished">Zobrazit přehrávač</translation>
+        <translation>Zobrazit přehrávač</translation>
     </message>
     <message>
         <source>SHOUTcast server did not respond</source>
-        <translation type="unfinished">SHOUTcast server neodpovídá</translation>
+        <translation>SHOUTcast server neodpovídá</translation>
     </message>
 </context>
 <context>
     <name>SubGenrePage</name>
     <message>
         <source>Reload</source>
-        <translation type="unfinished">Obnovit</translation>
+        <translation>Obnovit</translation>
     </message>
     <message>
         <source>SHOUTcast server did not respond</source>
-        <translation type="unfinished">SHOUTcast server neodpovídá</translation>
+        <translation>SHOUTcast server neodpovídá</translation>
     </message>
 </context>
 <context>
     <name>TopStationsPage</name>
     <message>
         <source>SHOUTcast server returned no Stations</source>
-        <translation type="unfinished">SHOUTcast server nenalezl stanice</translation>
+        <translation>SHOUTcast server nenalezl stanice</translation>
     </message>
     <message>
         <source>Show Player</source>
-        <translation type="unfinished">Zobrazit přehrávač</translation>
+        <translation>Zobrazit přehrávač</translation>
     </message>
     <message>
         <source>Reload</source>
-        <translation type="unfinished">Obnovit</translation>
+        <translation>Obnovit</translation>
     </message>
     <message>
         <source>Top 500</source>
@@ -279,26 +279,26 @@
     </message>
     <message>
         <source>No stations found</source>
-        <translation type="unfinished">Stanice nenalezeny</translation>
+        <translation>Stanice nenalezeny</translation>
     </message>
     <message>
         <source>SHOUTcast server did not respond</source>
-        <translation type="unfinished">SHOUTcast server neodpovídá</translation>
+        <translation>SHOUTcast server neodpovídá</translation>
     </message>
 </context>
 <context>
     <name>shoutcast-sailfish</name>
     <message>
         <source>Audio Player:</source>
-        <translation type="unfinished">Audio přehrávač:</translation>
+        <translation>Audio přehrávač:</translation>
     </message>
     <message>
         <source>Don&apos;t know how to retrieve playlist.</source>
-        <translation type="unfinished">Nevím jak načíst playlist.</translation>
+        <translation>Nevím jak načíst playlist.</translation>
     </message>
     <message>
         <source>Failed to retrieve stream URL.</source>
-        <translation type="unfinished">Nepodařilo se načíst URL streamu.</translation>
+        <translation>Nepodařilo se načíst URL streamu.</translation>
     </message>
     <message>
         <source>Server did not respond while retrieving stream URL.</source>
